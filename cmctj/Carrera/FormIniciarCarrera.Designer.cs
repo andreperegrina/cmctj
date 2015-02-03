d1b430110aed5c9044f8a9a524bb9b0fec7fb661
andreperegrina <andre.peregrina@gmail.com>
2015-02-02 19:00:03 -0800

    Iniciar carrera

---
+++ cmctj/Carrera/FormIniciarCarrera.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace cmctj.Carrera
+{
+    partial class FormIniciarCarrera
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.sglueDxCategoria = new DevExpress.XtraEditors.SearchLookUpEdit();
+            this.searchLookUpEdit1View = new DevExpress.XtraGrid.Views.Grid.GridView();
+            ((System.ComponentModel.ISupportInitialize)(this.sglueDxCategoria.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.searchLookUpEdit1View)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCerrar.Location = new System.Drawing.Point(221, 86);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(75, 23);
+            this.btnCerrar.TabIndex = 7;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(302, 86);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(75, 23);
+            this.btnAceptar.TabIndex = 6;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Categoría";
+            // 
+            // sglueDxCategoria
+            // 
+            this.sglueDxCategoria.EditValue = "Vacio";
+            this.sglueDxCategoria.Location = new System.Drawing.Point(12, 40);
+            this.sglueDxCategoria.Name = "sglueDxCategoria";
+            this.sglueDxCategoria.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.sglueDxCategoria.Properties.DisplayMember = "Nombre";
+            this.sglueDxCategoria.Properties.ValueMember = "CategoriaId";
+            this.sglueDxCategoria.Properties.View = this.searchLookUpEdit1View;
+            this.sglueDxCategoria.Size = new System.Drawing.Size(365, 20);
+            this.sglueDxCategoria.TabIndex = 8;
+            // 
+            // searchLookUpEdit1View
+            // 
+            this.searchLookUpEdit1View.FocusRectStyle = DevExpress.XtraGrid.Views.Grid.DrawFocusRectStyle.RowFocus;
+            this.searchLookUpEdit1View.Name = "searchLookUpEdit1View";
+            this.searchLookUpEdit1View.OptionsSelection.EnableAppearanceFocusedCell = false;
+            this.searchLookUpEdit1View.OptionsView.ShowGroupPanel = false;
+            // 
+            // FormIniciarCarrera
+            // 
+            this.AcceptButton = this.btnAceptar;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCerrar;
+            this.ClientSize = new System.Drawing.Size(397, 131);
+            this.Controls.Add(this.sglueDxCategoria);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "FormIniciarCarrera";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "FormIniciarCarrera";
+            ((System.ComponentModel.ISupportInitialize)(this.sglueDxCategoria.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.searchLookUpEdit1View)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Label label1;
+        private DevExpress.XtraEditors.SearchLookUpEdit sglueDxCategoria;
+        private DevExpress.XtraGrid.Views.Grid.GridView searchLookUpEdit1View;
+    }
+}
---
+++ cmctj/Carrera/FormIniciarCarrera.cs
@@ -0,0 +1,44 @@
+﻿using cmctj.Utils;
+using CMCTJ.BusinessEntity;
+using CMCTJ.BusinessLogic.Manager;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cmctj.Carrera
+{
+    public partial class FormIniciarCarrera : Form
+    {
+        public FormIniciarCarrera()
+        {
+            InitializeComponent();
+            WrapperManager nuevoWrapperManager = new WrapperManager();
+            sglueDxCategoria.Properties.DataSource = nuevoWrapperManager.GetAllCategoriaWrapper();
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            CarreraInicioCategoriaManager nuevoManager = new CarreraInicioCategoriaManager();
+            int? carreraID = (int?)SessionData.Instance["carrera_actual_id"];
+            carrera_inicio_categoria nuevoInicioCarrera = new carrera_inicio_categoria()
+            {
+                carrera_id = carreraID.Value,
+                 categoria_id=(int)sglueDxCategoria.EditValue,
+                 tiempo_inicio_carrera=DateTime.Now
+            };
+
+            nuevoManager.Save(nuevoInicioCarrera);
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
